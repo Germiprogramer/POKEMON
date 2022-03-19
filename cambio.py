abaa5c17b80e62bbc31a2a8a49c0a09363be9d85
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-19 18:45:21 +0100

    cambio no funcional

---
+++ cambio.py
@@ -0,0 +1,27 @@
+from classes.pokemon import *
+from classes.pokemon_planta import *
+from classes.pokemon_electrico import *
+from classes.pokemon_agua import *
+from classes.pokemon_fuego import *
+from combate import *
+from eficacias import *
+
+pokemon_1 = ""
+pokemon_2 = ""
+
+entrenador1_pokemon1 = Pokemon_planta(1, "bulbasaur", 50, 8, 10)
+entrenador1_pokemon2= Pokemon_agua(4, "squirtle", 45, 9, 11)
+entrenador2_pokemon1= Pokemon_fuego(7, "charmander", 47, 11, 7)
+entrenador2_pokemon2= Pokemon_electrico(25, "pikachu", 43, 12, 8)
+
+pokemonentrenador1 = [entrenador1_pokemon1, entrenador1_pokemon2]
+pokemonentrenador2 = [entrenador2_pokemon1, entrenador2_pokemon2]
+
+print(pokemonentrenador1[0].ID)
+
+def cambio_pok_1(pokemonentrenador1):
+    pokemon_1 = pokemonentrenador1[0]
+    return pokemon_1
+
+pokemon_1 = cambio_pok_1(pokemonentrenador1[0])
+print(pokemon_1.ID)
