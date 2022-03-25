2d7815671f989d2e3f72a487c6bd2cc18e11871c
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-25 10:04:28 +0100

    commit algo falla en la condicion cambio

--- practicas.py
+++ practicas.py
@@ -1,5 +1,3 @@
-import csv
-
 from classes.pokemon import *
 from classes.pokemon_planta import *
 from classes.pokemon_electrico import *
@@ -8,8 +6,34 @@ from classes.pokemon_fuego import *
 from combate import *
 from eficacias import *
 
-pokemon_1 = Pokemon_planta(1, "bulbasaur", 50, 8, 10)
-pokemon_2 = Pokemon_agua(4, "squirtle", 45, 12, 7)
+entrenador1_pokemon1 = Pokemon_planta(1, "bulbasaur", 50, 8, 10, 9)
+entrenador1_pokemon2= Pokemon_agua(4, "squirtle", 45, 9, 11, 10)
+entrenador2_pokemon1= Pokemon_fuego(7, "charmander", 47, 11, 7, 11)
+entrenador2_pokemon2= Pokemon_electrico(25, "pikachu", 43, 12, 8, 13)
 
-pokemon_2.attack(pokemon_1)
-print(pokemon_1.health_points)
+pokemonentrenador1 = [entrenador1_pokemon1, entrenador1_pokemon2]
+pokemonentrenador2 = [entrenador2_pokemon1, entrenador2_pokemon2]
+
+def combateconcambio(pokemonentrenador1, pokemonentrenador2):
+    i = 0
+    j = 0
+
+    if pokemonentrenador1[i].get_health_points() <0:
+        i = i + 1
+    if pokemonentrenador2[j].get_health_points() <0:
+        j = j + 1
+
+
+    
+    pokemonentrenador1[i].eficacias(pokemonentrenador2[j])
+    pokemonentrenador2[j].eficacias(pokemonentrenador1[i])
+    combate(pokemonentrenador1[i], pokemonentrenador2[j])
+    if pokemonentrenador1[i].get_health_points() >0 and pokemonentrenador2[j].get_health_points()>0:
+        try:
+            combateconcambio(pokemonentrenador1, pokemonentrenador2)
+        except:
+            pass
+    else:
+        pass
+
+combateconcambio(pokemonentrenador1, pokemonentrenador2)
