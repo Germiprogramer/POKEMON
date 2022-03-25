4acddecbcb3f028bf4fe3a62f8414068ffa17ada
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-25 17:03:14 +0100

    cpommit

--- cambio.py
+++ cambio.py
@@ -4,18 +4,9 @@ from classes.pokemon_electrico import *
 from classes.pokemon_agua import *
 from classes.pokemon_fuego import *
 from combate import *
-from eficacias import *
 
 pokemon_1 = ""
 pokemon_2 = ""
-
-entrenador1_pokemon1 = Pokemon_planta(1, "bulbasaur", 50, 8, 10, 9)
-entrenador1_pokemon2= Pokemon_agua(4, "squirtle", 45, 9, 11, 10)
-entrenador2_pokemon1= Pokemon_fuego(7, "charmander", 47, 11, 7, 11)
-entrenador2_pokemon2= Pokemon_electrico(25, "pikachu", 43, 12, 8, 13)
-    
-pokemonentrenador1 = [entrenador1_pokemon1, entrenador1_pokemon2]
-pokemonentrenador2 = [entrenador2_pokemon1, entrenador2_pokemon2]
 
 def combateconcambio(pokemonentrenador1, pokemonentrenador2):
     i = 0
@@ -25,12 +16,14 @@ def combateconcambio(pokemonentrenador1, pokemonentrenador2):
             pass
     else:
         if pokemonentrenador1[i].get_health_points() <0:
+            print("¡CAMBIO DE POKEMON, ENTRA {} Y SALE {}!".format(pokemonentrenador1[i+1].get_pokemon_name(),pokemonentrenador1[i].get_pokemon_name()))
             i = i + 1
     
     if j >= len(pokemonentrenador2):
             pass
     else:
         if pokemonentrenador2[j].get_health_points() <0:
+            print("¡CAMBIO DE POKEMON, ENTRA {} Y SALE {}!".format(pokemonentrenador2[i+1].get_pokemon_name(),pokemonentrenador2[i].get_pokemon_name()))
             j = j + 1
 
     if pokemonentrenador1[i].get_health_points() >0 and pokemonentrenador2[j].get_health_points()>0:
